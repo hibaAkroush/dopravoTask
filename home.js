78f269eea098302285e2185ba34df64bf0fef40a
hibaAkroush <hiba.akr@gmail.com>
2018-02-09 17:28:34 +0200

    slick finally works

--- home.js
+++ home.js
@@ -3,25 +3,25 @@ function load() {
 var data = elems.elements
 console.log(data)
 
+
 _.each(data,function(val,i){
 console.log(val.background)
 var box = '<div id = "box"></div>'
 var p1img = '<img src =' + val.playerOneImg +' id="playerOneImg"/>'
-var p2img = '<img src =' + val.playerTwoImg + 'id="playerTwoImg"/>'
-var titlelogo = '<img src =' + val.titleLogo + 'id="titleLogo"/>'
+var p2img = '<img src =' + val.playerTwoImg + ' id="playerTwoImg"/>'
+var titlelogo = '<img src =' + val.titleLogo + ' id="titleLogo"/>'
 var p1name = '<p>'+val.playerOneName+'</p>'
 var p2name = '<p>'+ val.playerTwoName +'</p>'
-
-// 		<p id="vs">{vs}</p>
-// 		<p id="title">{title}</p>
-// 		<p id="date">{date}</p>
-// 		<p id="time">{time}</p>
-//      <img id="loc" src={loc}/>
-// 		<p id="place">{place}</p>
-// 		<p id="event">{event}</p>
-// 		<p id="seats">{seatsRemaining} Remaining of {seatsTotal} Seats</p>
-
-$("#element"+i).append(p2name, p1name)
+var vs = '<p id="vs">' + val.vs + '</p>'
+var title ='<p id="title">' + val.title + '</p>'
+var date = '<p id="date">' + val.date + '</p>'
+var time = '<p id="time">' + val.time + '</p>'
+var place = '<p id="place">' + val.place + '</p>'
+var event = '<p id="event">' + val.event + '</p>'
+var seats = '<p id="seats">' + val.seatsRemaining +  'Remaining of'  + val.seatsTotal +  'Seats</p>'
+var background = '<img src =' + val.background +' id="backgroundImg"/>'
+var coming = '<img src =' + val.coming +' id="coming"/>'
+$("#element"+i).append(coming, background, seats, event, place, time, date, title, vs,titlelogo, box, p2img, p1img, p2name, p1name)
 	})
 
 $('.multiple-items').slick({
@@ -34,39 +34,3 @@ $('.multiple-items').slick({
 });
 }
 
-// const backgroundImg = props.elem.background
-// 	const playerOneName =props.elem.playerOneName
-// 	const playerOneImg=props.elem.playerOneImg
-// 	const playerTwoName=props.elem.playerTwoName
-// 	const playerTwoImg=props.elem.playerOneImg
-// 	const vs=props.elem.vs
-// 	const title=props.elem.title
-// 	const titleLogo=props.elem.titleLogo
-// 	const coming=props.elem.coming
-// 	const date=props.elem.date
-// 	const time=props.elem.time
-// 	const place=props.elem.place
-// 	const event=props.elem.event
-// 	const seatsRemaining=props.elem.seatsRemaining
-// 	const seatsTotal =props.elem.seatsTotal
-// 	return (
-// 		<div>
-// 		<img src = {backgroundImg} id="backgroundImg"/>
-// 		<div id = "box"></div>
-// 		<img src = {playerOneImg} id="playerOneImg"/>
-// 		<img src = {playerTwoImg} id="playerTwoImg"/>
-// 		<img src = {titleLogo} id="titleLogo"/>
-// 		<img src = {coming} id="coming"/>
-// 		<p id="playerOneName">{playerOneName}</p>
-// 		<p id="playerTwoName">{playerTwoName}</p>
-// 		<p id="vs">{vs}</p>
-// 		<p id="title">{title}</p>
-// 		<p id="date">{date}</p>
-// 		<p id="time">{time}</p>
-//      <img id="loc" src={loc}/>
-// 		<p id="place">{place}</p>
-// 		<p id="event">{event}</p>
-// 		<p id="seats">{seatsRemaining} Remaining of {seatsTotal} Seats</p>
-
-// 		</div>
-// 		)
